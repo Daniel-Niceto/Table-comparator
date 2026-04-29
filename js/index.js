d3fe79b3e0db4b19539f35dba3f0746329fd5279
Daniel-Niceto <149837207+Daniel-Niceto@users.noreply.github.com>
2026-04-29 12:56:29 +0000

    se añaden colores

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const textForms = document.querySelectorAll(`.Text-form`)
 
 // const body = document.querySelector(`body`)
 // const tableWrapper = document.querySelector(`.Table`)
-const tableBody = document.querySelector(`.Table-body`)
+// const table = document.querySelector(`.Table`)
 
 
 
@@ -20,7 +20,6 @@ const handleButtonClick = () => {
         const tableData = textForm.value.split(`\n`)
 
         tableData.forEach((row) => {
-
             let x = []
             x = row.split(`\t`)
             const rows = {
@@ -52,7 +51,6 @@ const formatTable = () => {
             }
             row++
         }
-
     })
 }
 
@@ -71,7 +69,6 @@ const addRow = (firstTable, secondTable) => {
         description_2: secondTable.description,
         units_2: secondTable.units,
     }
-
     combinedTables.push(newRow)
 }
 
@@ -82,55 +79,72 @@ const compareTable = () => {
     while (true) {
         if (index_t1 == tables[0].length && index_t2 == tables[1].length) {
             return
-        } else if (index_t1 == tables[0].length){
-            while(index_t2 < tables[1].length){
-                addRow(rowMissing(),tables[1][index_t2])
-
+        } else if (index_t1 == tables[0].length) {
+            while (index_t2 < tables[1].length) {
+                addRow(rowMissing(), tables[1][index_t2])
                 index_t2++
             }
             return
-        }else if(index_t2 === tables[1].length){
-            while(index_t1 < tables[0].length){
+        } else if (index_t2 === tables[1].length) {
+            while (index_t1 < tables[0].length) {
                 addRow(tables[0][index_t1], rowMissing())
-
                 index_t1++
             }
             return
         }
-
 
         const codeTable_1 = tables[0][index_t1].code
         const codeTable_2 = tables[1][index_t2].code
 
         if (compareCodes(codeTable_1, codeTable_2) == 0) {
             addRow(tables[0][index_t1], tables[1][index_t2])
-
             index_t1++
             index_t2++
 
         } else if (compareCodes(codeTable_1, codeTable_2) < 0) {
             addRow(tables[0][index_t1], rowMissing())
-
             index_t1++
         } else {
             addRow(rowMissing(), tables[1][index_t2])
-
             index_t2++
         }
     }
 }
 
 const rowMissing = () => {
-    return { "code": "missing", "description": "missing", "units": "missing" }
+    return { "code": "", "description": "", "units": "" }
 }
 
 const printTable = () => {
 
+    const table = document.createElement(`table`)
+    const location = document.querySelector(`main`)
+    table.classList.add(`Table`)
+    table.innerHTML = `
+    <thead class="Table-head Head">
+        <tr class="Head-row">
+            <th class="Head-data" colspan="3">Table 1</th>
+            <th class="Head-data" colspan="3">Table 2</th>
+        </tr>
+        <tr class="Head-row">
+            <th class="Head-data">Code table 1</th>
+            <th class="Head-data">Description table 1</th>
+            <th class="Head-data">Units table 1</th>
+            <th class="Head-data">Code table 2</th>
+            <th class="Head-data">Description table 2</th>
+            <th class="Head-data">Units table 2</th>
+        </tr>
+    </thead>
+    <tbody class="Table-body Body">
+    </tbody>
+    `
+
+    location.appendChild(table)
     const tableBody = document.querySelector(`.Table-body`)
 
     combinedTables.forEach((row) => {
-
         const tableRow = document.createElement(`tr`)
+        tableRow.classList.add(`Body-row`)
 
         tableRow.innerHTML = `
                 <td class="Body-data">${row.code_1}</td>
@@ -140,17 +154,14 @@ const printTable = () => {
                 <td class="Body-data">${row.description_2}</td>
                 <td class="Body-data">${row.units_2}</td>
             `
-            // console.log(tableRow)
         tableBody.appendChild(tableRow)
-
-
     })
 }
 
-const inputsAreEmpty = () =>{
+const inputsAreEmpty = () => {
     let empty = false
-    textForms.forEach((textForm)=>{
-        if(textForm.value.length === 0 && tables.length === 0){
+    textForms.forEach((textForm) => {
+        if (textForm.value.length === 0 && tables.length === 0) {
             empty = true
         }
     })
@@ -158,35 +169,55 @@ const inputsAreEmpty = () =>{
 }
 
 const addStyles = () => {
-    const cells = document.querySelectorAll(`.Body-data`)
-    cells.forEach((cell)=>{
-        if(cell.innerHTML == "missing"){
-            console.log(cell)
-            cell.classList.add(`isMissing`)
+    // const cells = document.querySelectorAll(`.Body-data`)
+    // cells.forEach((cell) => {
+    //     if (cell.innerHTML == "missing") {
+    //         cell.classList.add(`isMissing`)
+    //     }
+    // })
+
+    const rows = document.querySelectorAll(`.Body-row`)
+    rows.forEach((row) => {
+        if (row.children[0].innerHTML == "" || row.children[3].innerHTML == "") {
+            row.classList.add(`isMissing`)
         }
     })
 }
 
-buttonForm.addEventListener(`click`, ()=>{
+const checkUnits = () => {
+    const rows = document.querySelectorAll(`.Body-row`)
+    rows.forEach((row)=>{
+        if(row.children[2].innerHTML != row.children[5].innerHTML){
+            row.children[2].classList.add(`notMatching`)
+            row.children[5].classList.add(`notMatching`)
+        }
+    })
+}
+
+buttonForm.addEventListener(`click`, () => {
 
     // Check that there is content in the inputs
     // If the inputs are empty it returns
-    if(inputsAreEmpty()){
+    if (inputsAreEmpty()) {
         return
     }
 
     // Cleans the array and the table shown in the window
-    tableBody.innerHTML = ``
+    const table = document.querySelector(`.Table`)
+    if (table) {
+        table.innerHTML = ``
+    }
+    tables = []
     combinedTables = []
 
 
     handleButtonClick()
     sortTable()
     formatTable()
-
     compareTable()
     printTable()
     addStyles()
 
-    // console.log(combinedTables)
+    tables = []
+    combinedTables = []
 })
